dd1d31de2dc256f1db1cc8ad73be3648fe243877
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-08-15 00:08:29 -0700

    Adds movement and boundaries to player character

--- js/app.js
+++ js/app.js
@@ -3,20 +3,22 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
     this.x = -101;
     this.y = 219;
-    this.speed = 10;
+    this.speed = 100;
 };
 
 let slowBug = new Enemy();
 let fastBug = new Enemy();
 let superfastBug = new Enemy();
 
-allEnemies = [slowBug]
+allEnemies = [slowBug];
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // Multiply any movement by the dt parameter which will ensure the game runs at the same speed for all computers.
-    this.x = this.x + this.speed * dt;
+  this.x = this.x + this.speed * dt;
+  if (this.x > 505) {
+    delete this.x;
+  }
 };
 
 // Draws the enemy on the screen, required method for game
@@ -27,16 +29,15 @@ Enemy.prototype.render = function() {
 // Define player class
 // Class requires update(), render() and handleInput() methods
 
-
 // Instantiate objects
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
 var Player = function () {
   this.sprite = 'images/char-horn-girl.png';
   this.x = 202;
   this.y = 385;
 }
+
+let player = new Player();
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -45,7 +46,18 @@ Player.prototype.render = function() {
 Player.prototype.update = function() {
 }
 
-let player = new Player();
+
+Player.prototype.handleInput = function(key) {
+  if(key === 'up' && this.y > 0){
+    this.y = this.y - 83;
+  } else if (key === 'down' && this.y < 385) {
+    this.y = this.y + 83;
+  } else if (key === 'right' && this.x < 404) {
+    this.x = this.x + 101;
+  } else if (key === 'left' && this.x > 0) {
+    this.x = this.x - 101;
+  }
+}
 
 // Listens for key presses and sends the keys to Player.handleInput()
 document.addEventListener('keyup', function(e) {
@@ -56,5 +68,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    // player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);
 });
